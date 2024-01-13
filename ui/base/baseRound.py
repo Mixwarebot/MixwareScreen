1bf95aca18bfc4275e1eb10e8978f3efeddcdd20
Mixwarebot <rd2@mixwarebot.com>
2024-01-13 15:30:28 +0800

    Update baseRound

--- ui/base/baseRound.py
+++ ui/base/baseRound.py
@@ -2,7 +2,7 @@ from qtCore import *
 
 
 class BaseRoundDialog(QDialog):
-    def __init__(self, parent, radius_x=10, radius_y=10, color="#909090", alpha=1.0):
+    def __init__(self, parent, radius_x=0, radius_y=0, color="#999999", alpha=1.0):
         super().__init__(parent)
         self.setWindowFlags(Qt.FramelessWindowHint | Qt.Tool)
         self.setAttribute(Qt.WA_TranslucentBackground)
@@ -20,6 +20,7 @@ class BaseRoundDialog(QDialog):
         rect.setWidth(rect.width())
         rect.setHeight(rect.height())
         painter.drawRoundedRect(rect, self.radius_x, self.radius_y)
+
 
 class BaseRoundWidget(QWidget):
     def __init__(self, parent, radius_x=10, radius_y=10, color="#FFFFFF", alpha=1.0):
